d90854f783b756b5edb891d5f89975fd21a70cd4
danieljarfas <101869116+danieljarfas@users.noreply.github.com>
2023-11-30 10:02:16 +0100

    Add files via upload

---
+++ ulesrend/view/login.php
@@ -0,0 +1,7 @@
+<form action="index.php" method="post">
+        Felhasználónév:<br>
+        <input type="text" name="felhasznalonev" value="" required><br>
+        Jelszó:<br>
+        <input type="password" name="jelszo" required><br>
+        <input type="submit" value="BELÉPÉS">
+    </form>

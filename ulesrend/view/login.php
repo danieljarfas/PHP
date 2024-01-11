ad11345b04d66a0ef99d98929d42e585d88cb98c
danieljarfas <101869116+danieljarfas@users.noreply.github.com>
2024-01-11 08:40:38 +0100

    Delete ulesrend directory

--- ulesrend/view/login.php
+++
@@ -1,7 +0,0 @@
-<form action="index.php" method="post">
-        Felhasználónév:<br>
-        <input type="text" name="felhasznalonev" value="" required><br>
-        Jelszó:<br>
-        <input type="password" name="jelszo" required><br>
-        <input type="submit" value="BELÉPÉS">
-    </form>
